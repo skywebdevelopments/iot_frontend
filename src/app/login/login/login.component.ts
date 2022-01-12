d98d72fcc9bc8d2053da9066c5b613a56663b57d
aasmaaayman <asmaa20171701014@cis.asu.edu.eg>
2022-01-12 15:14:37 +0200

    Email-Validation

--- src/app/login/login/login.component.ts
+++ src/app/login/login/login.component.ts
@@ -25,7 +25,7 @@ export class LoginComponent implements OnInit {
     this.form_login = this.formBuilder.group({
       email: ['', Validators.compose([
         Validators.required,
-        Validators.pattern('^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$')
+        Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')
       ])],
       password: ['', Validators.compose([
         Validators.required
