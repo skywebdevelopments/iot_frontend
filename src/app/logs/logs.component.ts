d3a864845d073d7f4f05c8c4fc431cf740e3a13b
enjy609 <enjyahmed95@gmail.com>
2022-03-20 20:45:12 +0200

    Logs-Tree

--- src/app/logs/logs.component.ts
+++ src/app/logs/logs.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit,ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
 import { LogService } from '../../app/service/log.service';
+import { ListLogService } from '../../app/service/list-log.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
 
 import { MatSort, MatSortable } from '@angular/material/sort';
@@ -13,9 +14,15 @@ export interface logElement {
   operation: "text",
   log_Message: "text",
   log_Level: "text",
-  user_email:"text",
-  createdAt:"text",
-  updatedAt:"text"
+  user_email: "text",
+  parent_rec_id: "text",
+  child_rec_id: "text",
+  createdAt: "text",
+  updatedAt: "text"
+}
+
+const TABLE_SCHEMA = {
+  "isEdit": "isEdit"
 }
 
 // to be filled from the service
@@ -27,34 +34,38 @@ const ELEMENT_DATA: logElement[] = [];
   styleUrls: ['./logs.component.css']
 })
 export class LogsComponent implements OnInit {
-   // form controls.
-   authorized: boolean;
-   // end
- 
- 
-   displayedColumns: string[] = ['operation', 'log_message', 'log_level', 'user_email','createdAt'];
-   dataSource = new MatTableDataSource<logElement>(ELEMENT_DATA);
-   
- 
-   @ViewChild(MatSort) sort: MatSort;
-   @ViewChild(MatPaginator) paginator: MatPaginator;
- 
+  // form controls.
+  authorized: boolean;
+  selectedid: any;
+  logss: any
+  selectedTableRecord: any
+  // end
 
-   ngAfterViewInit() {
+
+  displayedColumns: string[] = ['operation', 'log_message', 'log_level', 'user_email', 'createdAt', 'isEdit'];
+  dataSource = new MatTableDataSource<logElement>(ELEMENT_DATA);
+  dataSchema = TABLE_SCHEMA;
+
+  @ViewChild(MatSort) sort: MatSort;
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
+
+  ngAfterViewInit() {
 
     this.dataSource.paginator = this.paginator;
 
-  } 
+  }
 
   constructor(
     private service_log: LogService,
+    private service_log_parentRecid: ListLogService,
     private _snackBar: MatSnackBar,
     private service_authorize: AuthorizeRoleService,
   ) { }
 
 
   applyFilter_buttonGroup(event: any) {
-    this.dataSource.filter =event.trim().toLowerCase();
+    this.dataSource.filter = event.trim().toLowerCase();
     if (this.dataSource.paginator) {
       this.dataSource.paginator.firstPage();
     }
@@ -79,8 +90,8 @@ export class LogsComponent implements OnInit {
 
 
   get_log_list(showSnackBar: boolean) {
-
     this.service_log.service_list_logs().then(res => {
+      //console.log(res['data'])
       if (res['data']) {
         // add data to the table (data source)
         this.dataSource.data = res['data']
@@ -94,18 +105,34 @@ export class LogsComponent implements OnInit {
           this.dataSource.data = [];
           this.openSnackBar("list is Empty!", "Ok", 2000);
         }
-       
+
       }
     });
   };
 
+  get_log_list_parentRecid(id: any) {
+    this.selectedid = id;
+    var logs = []
+    this.service_log_parentRecid.service_list_logs_parentrecid({ "parent_rec_id": id }).then(res => {
+      console.log(res['data'])
+      res['data'].forEach(function (log) {
+        logs.push(log)
+      });
+    });
+    this.logss=logs
+    return logs;
+  };
+
+  selectTableRow(row: any) {
+    this.selectedTableRecord = row;
+
+  }
 
 
   ngOnInit(): void {
-    
     this.get_log_list(false);
   }
-    authorize(role) {
+  authorize(role) {
     return this.service_authorize.service_authorize_user(role);
   }
 
