3fb3a76501a9c4623f3cab46e8cd5fe8ca231c0c
enjy609 <enjyahmed95@gmail.com>
2022-03-27 14:42:24 +0200

    Node-Types Chart

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ChartType } from "angular-google-charts";
-import { ChartOptions, Chart} from 'chart.js';
+import { ChartOptions, Chart } from 'chart.js';
 import { LogService } from '../../app/service/log.service';
+import { ListEntityService } from '../../app/service/entity/list-entity.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
 
 
@@ -14,11 +15,21 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 export class DashboardComponent implements OnInit {
 
   myType = ChartType.PieChart;
-  element:any;
+
+  //logs
+  element: any;
   info: Array<string> = [];
   trace: Array<string> = [];
   error: Array<string> = [];
   myData = [];
+
+  //entity
+  entity: any;
+  Light: Array<string> = [];
+  Temperature: Array<string> = [];
+  Humidity: Array<string> = [];
+  myentity = [];
+
   options = {
     colors: ['#0ee023', '#1221a6', '#bd1111', '#f3b49f', '#f6c7b6'],
     is3D: true
@@ -26,11 +37,15 @@ export class DashboardComponent implements OnInit {
   width = 1050;
   height = 600;
 
-  constructor(private service_log: LogService, private _snackBar: MatSnackBar) { }
+  constructor(private service_log: LogService,
+    private _snackBar: MatSnackBar,
+    private service_listentity: ListEntityService,
+  ) { }
 
   ngOnInit(): void {
     this.get_log_list(true);
-  } 
+    this.get_entity_list(true);
+  }
 
   openSnackBar(message: string, action: string, interval: number) {
     this._snackBar.open(message, action);
@@ -44,13 +59,13 @@ export class DashboardComponent implements OnInit {
       if (res['data']) {
         for (let i of res['data']) {
           this.element = i.log_level;
-          if(this.element == 'INFO'){
+          if (this.element == 'INFO') {
             this.info.push(this.element);
           }
-          else if(this.element == 'ERROR'){
+          else if (this.element == 'ERROR') {
             this.error.push(this.element);
           }
-          else if(this.element == 'TRACE'){
+          else if (this.element == 'TRACE') {
             this.trace.push(this.element);
           }
         }
@@ -66,13 +81,34 @@ export class DashboardComponent implements OnInit {
       ];
     });
   };
-  
 
-  // private getChartData() {
-  //   this.appService.getChartData().subscribe(data => {
-  //     console.log(data);
-  //     this.chartData = data;
-  //   })
-  // }
+
+  get_entity_list(showSnackBar: boolean) {
+    this.service_listentity.service_list_entity().then(res => {
+      if (res['data']) {
+        for (let i of res['data']) {
+          this.entity = i.type;
+          if (this.entity == 'Light') {
+            this.Light.push(this.entity);
+          }
+          else if (this.entity == 'Temperature') {
+            this.Temperature.push(this.entity);
+          }
+          else if (this.entity == 'Humidity') {
+            this.Humidity.push(this.entity);
+          }
+        }
+        // display a notification
+        if (showSnackBar) {
+          this.openSnackBar("Done!", "Ok", 2000);
+        }
+      }
+      this.myentity = [
+        ['Light', this.Light.length],
+        ['Temperature', this.Temperature.length],
+        ['Humidity', this.Humidity.length],
+      ];
+    });
+  };
 
 }
